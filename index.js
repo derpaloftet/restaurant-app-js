9198ecb01859226ee68e5b5e8f9d3f16c553cd6b
Yuli <yulia.yurchenko90@gmail.com>
2025-01-13 16:13:22 +0100

    - code refactorings - css optimization for mobile version

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const orderEl = document.getElementById("order-list-info")
 const totalPriceEl = document.getElementById("total-price")
 const paymentEl = document.getElementById("payment-modal")
 const submitFormEl = document.getElementById("submit-form")
-const orderConfEl = document.getElementById("order-confirmation")
+const orderConfirmationEl = document.getElementById("order-confirmation")
 
 const foodItem = document.createElement("ul")
 foodItem.innerHTML = menuArray.map(item => {
    return `  
                     <li>
-                      <div class="item">
-                        <div class="item-emoji">${ item.emoji }</div>
-                      </div>
-                      <div class="item item-details">
+                      <div class="item-emoji">${ item.emoji }</div>
+                      <div class="item-details">
                         <div class="item--name">${ item.name }</div>
                         <div class="item--ingredients">${ item.ingredients.join(", ") }</div>
                         <div class="item--price">$${ item.price }</div>
                       </div>
-                      <div class="item item-btns">
+                      <div class="item-btns">
                         <button class="plus--btn" id="plus--btn-${ item.id }">+</button>
                         <button class="minus--btn" id="minus--btn-${ item.id }">-</button>
                       </div>
@@ -29,94 +27,98 @@ foodItem.innerHTML = menuArray.map(item => {
 }).join(" ")
 listEl.appendChild(foodItem)
 
-const listArr = []
+const orderItems = []
 let totalPrice = 0
 
+// add plus and minus buttons
 menuArray.forEach(item => {
         const plusBtnEl = document.getElementById(`plus--btn-${ item.id }`)
         plusBtnEl.addEventListener("click", () => {
             let hasItem = false
-            listArr.forEach(listObj => {
+            orderItems.forEach(listObj => {
                 if (listObj.id === item.id) {
                     hasItem = true
                 }
             })
             if (!hasItem) {
-                listArr.push({
+                orderItems.push({
                     id: item.id,
                     name: item.name,
                     price: item.price,
                     itemsSum: 1
                 })
             } else {
-                listArr.map(objOrder => {
-                    if (objOrder.id === item.id) {
-                        objOrder.itemsSum++
-                        objOrder.price = item.price * objOrder.itemsSum
+                orderItems.map(orderItem => {
+                    if (orderItem.id === item.id) {
+                        orderItem.itemsSum++
+                        orderItem.price = item.price * orderItem.itemsSum
                     }
                 })
             }
             totalPrice += item.price
-            plusBtnHandler()
+            showOrderSection()
         })
-    const minusBtnEl = document.getElementById(`minus--btn-${ item.id }`)
-    minusBtnEl.addEventListener("click", () => {
-        listArr.map(listObj => {
-            if (listObj.id === item.id && listObj.itemsSum > 0) {
-                listObj.itemsSum--
-                listObj.price = listObj.price - item.price
-                totalPrice -= item.price
-            }
+
+        const minusBtnEl = document.getElementById(`minus--btn-${ item.id }`)
+        minusBtnEl.addEventListener("click", () => {
+            orderItems.map(orderItem => {
+                if (orderItem.id === item.id && orderItem.itemsSum > 0) {
+                    orderItem.itemsSum--
+                    orderItem.price = orderItem.price - item.price
+                    totalPrice -= item.price
+                }
+            })
+            rerenderOrder()
         })
-        plusBtnHandler()
-    })
     }
 )
-function updateList() {
-    const listItemsEl = document.getElementById("list-items")   
-    listItemsEl.innerHTML = ""
-    console.log(listArr)
 
-    listArr.forEach(item => {
-        const index = listArr.indexOf(item)
+function rerenderOrder() {
+    const orderListItemsEl = document.getElementById("order-list-items")
+    orderListItemsEl.innerHTML = ""
+
+    for (let i = 0; i < orderItems.length; i++) {
+        const item = orderItems[i];
         const listItem = document.createElement("div")
-        listItem.innerHTML += `
+        listItem.innerHTML = `
          <div class="list-name-price">
            <div class="list-name">${ item.name }</div>
-           <button class="remove-btn" id="remove-btn-${ index }">remove</button>
+           <button class="remove-btn" id="remove-btn-${ i }">remove</button>
            <div class="list-itemsSum">${ item.itemsSum }</div>
            <div class="list-price">$${ item.price }</div>
          </div>
-         `  
-         listItemsEl.appendChild(listItem)
-         
-         const removeBtnEl = document.getElementById(`remove-btn-${index}`)
+         `
+        orderListItemsEl.appendChild(listItem)
+
+         const removeBtnEl = document.getElementById(`remove-btn-${i}`)
          removeBtnEl.addEventListener("click", ()=>{
-            listArr.splice(index, 1);
-            
-             if (listArr.length === 0) {
+            orderItems.splice(i, 1);
+             if (orderItems.length === 0) {
                  orderEl.style.display = "none"
              }
              totalPrice = totalPrice - item.price
-             updateList()
+             rerenderOrder()
          })
-         })
-    
-         totalPriceEl.innerHTML = `<div class="total-price-final">
+    }
+
+    totalPriceEl.innerHTML = `<div class="total-price-final">
          <div>Total price: </div>
          <div>$${totalPrice}</div>
          </div>` 
      }
 
-function plusBtnHandler() {
-    orderEl.style.display = "block"
-      updateList()
-      const orderBtnEl = document.getElementById("order--btn")
-      orderBtnEl.addEventListener("click", ()=>{
-          paymentEl.style.display = "flex"
-      })
-    orderConfEl.style.display = "none"
-    orderConfEl.innerHTML = ""
+function showOrderSection() {
+    // show order
+    orderEl.style.display = "flex"
+    rerenderOrder()
+    const orderBtnEl = document.getElementById("order--btn")
+    orderBtnEl.addEventListener("click", () => {
+        // show payment
+        paymentEl.style.display = "flex"
+    })
+    // hide confirmation
+    orderConfirmationEl.style.display = "none"
+    orderConfirmationEl.innerHTML = ""
 }
        
 submitFormEl.addEventListener("submit", (e) =>{
@@ -125,10 +127,10 @@ submitFormEl.addEventListener("submit", (e) =>{
               const nameData = formData.get('name')
               const confEl = document.createElement("div")
               confEl.innerHTML = `Thanks, ${nameData}! Your order is on it's way!`
-              orderConfEl.appendChild(confEl)
-              orderConfEl.style.display = "block"
+              orderConfirmationEl.appendChild(confEl)
+              orderConfirmationEl.style.display = "block"
               paymentEl.style.display = "none"
               orderEl.style.display = "none"
-              listArr.length = 0
+              orderItems.length = 0
               totalPrice = 0
  })
